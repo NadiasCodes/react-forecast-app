044a032b12172b6867771f8c4dbd41c7f75ac213
NadiasCodes <De.nadia@outlook.com>
2024-01-21 21:21:24 +0100

    created a weather app with ajax and api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
 import Weather from "./Weather";
+import "./index.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <Weather />
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className="App">
+    <h1>Weather Engine</h1>
+    <Weather />
+  </div>;
 }
 
 export default App;
